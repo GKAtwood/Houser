ceb82586dbd2828b1117133f9ef2c59877d44ad5
GKAtwood <gabrielle.k85@gmail.com>
2020-07-14 19:16:19 -0600

    commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,11 @@ class Header extends Component{
     render(){
         return(
 
-            <div>Header</div>
+            <header className='header'>
+                
+            <img src= 'https://raw.githubusercontent.com/DevMountain/simulation-2/master/assets/houser_logo.png' alt='logo'/>
+            <h1>Houser</h1>
+            </header>
 
 
         )
--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -21,25 +21,26 @@ class Wizard extends Component{
         return(
 
             <for className ='wizard'>
-                <img src= 'https://raw.githubusercontent.com/DevMountain/simulation-2/master/assets/houser_logo.png' alt='logo'/>
+               
                 <h1>HOUSER</h1>
+                
                  <div className='form'>
-                Address: <input type="text" value={this.state.address} onChange={e=>this.handleChange(e,"address")} />
-                </div>
-                <div className='form'>
-               City: <input type="text" value= {this.state.city} onChange={e=>this.handleChange(e,"city")}/>
-                </div>
-                <div className='form'>
-                State: <input type="text" value={this.state.state} onChange={e=>this.handleChange(e,"state")} />
-                </div>
-                <div className='form'>
-               Zipcode: <input type="text" value= {this.state.zipcode} onChange={e=>this.handleChange(e,"zipcode")}/>
-                </div>
-                <Link to= '/'>
+                     <h2>Add New Listing</h2>
+                     <Link to= '/'>
                 <div className='button-container'>
             <button className='black-button'>Cancel</button>
             </div>
             </Link>
+                Address: <input type="text" value={this.state.address} onChange={e=>this.handleChange(e,"address")} />
+                
+               
+               City: <input type="text" value= {this.state.city} onChange={e=>this.handleChange(e,"city")}/>
+             
+                
+                State: <input type="text" value={this.state.state} onChange={e=>this.handleChange(e,"state")} />
+                 Zipcode: <input type="text" value= {this.state.zipcode} onChange={e=>this.handleChange(e,"zipcode")}/>
+                </div>
+                
 
             </for>
  
